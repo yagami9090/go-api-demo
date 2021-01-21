2a95fa8159c71facc33a82bf59a6468815563ec3
Supod Camsopa <supod.c@cdg.co.th>
2021-01-21 14:19:53 +0700

    first commit

--- index.go
+++ index.go
@@ -15,6 +15,12 @@ func GetHelloWorld(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Hello World"})
 }
 
+func GetUserName(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+	c.JSON(http.StatusOK, gin.H{"username": username, "password": password})
+}
+
 func Post(c *gin.Context) {
 	var u User
 	c.BindJSON(&u)
@@ -22,20 +28,22 @@ func Post(c *gin.Context) {
 }
 
 func PutPathParameter(c *gin.Context) {
-	user := c.Param("user")
-	c.JSON(http.StatusOK, gin.H{"message": user})
+	username := c.Param("username")
+	password := c.Param("password")
+	c.JSON(http.StatusOK, gin.H{"username": username, "password": password})
 }
 
 func DELETEQUERY(c *gin.Context) {
-	user := c.Query("user")
-	c.JSON(http.StatusOK, gin.H{"message": user})
+	username := c.Query("username")
+	c.JSON(http.StatusOK, gin.H{"message": username})
 }
 
 func main() {
 	r := gin.Default()
 	r.GET("/", GetHelloWorld)
+	r.GET("/username", GetUserName)
 	r.POST("/", Post)
-	r.PUT("/", PutPathParameter)
-	r.DELETE("/", DELETEQUERY)
+	r.PUT("/user/:username/:password", PutPathParameter)
+	r.DELETE("/username", DELETEQUERY)
 	r.Run()
 }
